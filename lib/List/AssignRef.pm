c3a94c9ba12bff2b1118058fddd5d7de93fe3f38
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-15 13:52:46 +0100

    bump version; changelog

--- lib/List/AssignRef.pm
+++ lib/List/AssignRef.pm
@@ -20,7 +20,7 @@ use constant {
 
 BEGIN {
 	$List::AssignRef::AUTHORITY = 'cpan:TOBYINK';
-	$List::AssignRef::VERSION   = '0.003';
+	$List::AssignRef::VERSION   = '0.004';
 }
 
 use Sub::Exporter -setup => {
